10260cbcab58c17b4fef2e84f0568f6746fe7029
Simon Robinson <simon@robinson.ac>
2020-10-19 17:48:59 +0100

    Allow link editing

--- blocked.js
+++ blocked.js
@@ -1,5 +1,35 @@
 const urlParams = new URLSearchParams(window.location.search);
-const targetLink = urlParams.get('u');
+var targetLink = urlParams.get('u');
+
+function editLink(navigateToEnd) {
+	const linkElement = document.querySelector('span');
+	linkElement.addEventListener('keydown', function(event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			linkElement.contentEditable = false;
+			if (event.metaKey) {
+				window.location.href = targetLink;
+			}
+		}
+	}, false);
+	linkElement.addEventListener('input', function(event) {
+		targetLink = this.textContent;
+		document.querySelector('#visit').setAttribute('href', targetLink);
+		window.history.replaceState(null, '', '?u=' + targetLink);
+	}, false);
+
+	linkElement.contentEditable = true;
+
+	if (navigateToEnd && linkElement.textContent.length > 0) {
+		const selection = window.getSelection();
+		const range = document.createRange();
+		range.setStart(linkElement, 1);
+		range.setEnd(linkElement, 1);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	}
+	linkElement.focus();
+}
 
 function copyLink() {
 	const copySource = document.createElement('textarea');
@@ -18,11 +48,14 @@ function copyLink() {
 document.addEventListener('DOMContentLoaded', function(){
 	if (targetLink) {
 		const linkElement = document.querySelector('span');
-		linkElement.innerHTML = '';
-		linkElement.appendChild(document.createTextNode(targetLink));
-		document.querySelector('a:first-child').setAttribute('href', targetLink);
-		document.querySelector('a:last-child').addEventListener('click', copyLink);
+		linkElement.textContent = targetLink;
+		linkElement.addEventListener('click', function() { editLink(false); });
+		document.querySelector('#visit').setAttribute('href', targetLink);
+		document.querySelector('#edit-icon').addEventListener('click', function() { editLink(true); });
+		document.querySelector('#edit-text').addEventListener('click', function() { editLink(true); });
+		document.querySelector('#copy').addEventListener('click', copyLink);
 	} else {
+		document.querySelector('#edit-icon').classList.add('hidden');
 		document.querySelector('p:last-child').classList.add('hidden');
 	}
 });
